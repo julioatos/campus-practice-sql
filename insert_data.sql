868e1ae667ab4fcfd0c539fbc50553c197a9d080
Julio Cesar Ambriz Quijada <julio-cesar.ambriz@atos.net>
2022-07-11 17:29:12 -0500

    Agregado archivo .sql insert_data

---
+++ insert_data.sql
@@ -0,0 +1,31 @@
+insert into Status values(1, 'Pendiente');
+insert into Status values(2, 'En Proceso');
+insert into Status values(3, 'Cancelado');
+insert into Status values(4, 'Finalizado');
+insert into Status values(5, 'En Pausa');
+
+select * from Status
+
+
+insert into Company values (1, 'IMC', '5151 W 29th St #2201 Greeley, Colorado(CO), 80634');
+insert into Company values (2, 'Atoz', 'Ardmore, Tennessee(TN), 38449 4226 Highgate Dr');
+insert into Company values (3, 'Disnei', 'Horn Lake, Mississippi(MS), 38637');
+
+insert into Employees values (1, 'Juan', 'Perez', 'juan@jmail.com', 9991808182, 9500, 1);
+insert into Employees values (2, 'Paco', 'Ochoa', 'paco@jmail.com', 9991808183, 8000, 2);
+insert into Employees values (3, 'Pedro', 'Fernandez', 'pedro@jmail.com', 9991808184, 12500, 3);
+insert into Employees values (4, 'Sofi', 'Hernandez', 'sofi@jmail.com', 9991808185, 11000, 1);
+insert into Employees values (5, 'Isabella', 'Smith', 'isabella@jmail.com', 9991808186, 9000, 1);
+insert into Employees values (6, 'Eduardo', 'Jimenez', 'eduardo@jmail.com', 9991808187, 11000, 2);
+insert into Employees values (7, 'Jose', 'Pavon', 'jose@jmail.com', 9991808188, 12000, 3);
+insert into Employees values (8, 'Pancho', 'Fernandez', 'pamcho@jmail.com', 9991808189, 12500, 3);
+insert into Employees values (9, 'Francisco', 'Fernandez', 'francisco@jmail.com', 9991808190, 25000, 2);
+insert into Employees values (10, 'Diego', 'Olivarez', 'diego@jmail.com', 9991808191, 9000, 3);
+
+
+insert into Projects values (1, 'Dainler Learning', '2/7/1995', '2/22/2050', '', 2);
+insert into Projects values (2, 'Provident Software', '9/15/2022', '2/28/2023', '', 1);
+insert into Projects values (3, 'DataAnlysis SoftCentral', '1/31/2023', '10/5/2023', '', 1);
+insert into Projects values (4, 'Migration', '5/2/2021', '7/25/2022', '1/1/2022', 4);
+insert into Projects values (5, 'Atoz Insight', '12/30/2022', '10/1/2024', '', 3);
+
