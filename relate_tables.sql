2ab3a72ffb8929ed77ee199a16e2c7d9bac37a7f
Julio Cesar Ambriz Quijada <julio-cesar.ambriz@atos.net>
2022-07-11 18:35:58 -0500

    Agregada tabla pivote al archivo sql relate_tables

--- relate_tables.sql
+++ relate_tables.sql
@@ -1,2 +1,11 @@
 alter table Employees add CompanyId int;
 alter table Employees add constraint FK_Company_Employees foreign key (CompanyId) references Company (Id);
+create table EmployeesProjects
+(
+	Id int,
+	EmployeeId int not null,
+	ProjectId int not null,
+	constraint PK_EmployeesProjects primary key(Id),
+	constraint FK_Employee foreign key (EmployeeId) references Employees(Id),
+	constraint FK_Project foreign key (ProjectId) references Projects(Id),
+);
