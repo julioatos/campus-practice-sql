53845aabd785b5c932747abcf0469cccf71da3b9
Julio Cesar Ambriz Quijada <julio-cesar.ambriz@atos.net>
2022-07-12 12:12:59 -0500

    Agreda funcion HasAnyProjects y se modifico el parametro 'employeeName' en la funcion GetLastProjectIdByEmployeeName

--- functions_sql.sql
+++ functions_sql.sql
@@ -1,4 +1,4 @@
-create function GetLastProjetIdByEmployeeName(@EmployeeName varchar)
+create function GetLastProjetIdByEmployeeName(@EmployeeName varchar(max))
 	returns int
 as
 begin
@@ -16,4 +16,22 @@ begin
 	select @ret = @projectId;
 	if(@ret is null) set @ret = 0;
 	return @ret;
+end;
+
+create function HasAnyProject(
+	@EmployeeName as varchar(max)
+)
+returns bit
+as
+begin
+	declare @projectsCount int;
+	declare @epId int;
+
+	select @epId = Id from Employees where FirstName = @EmployeeName;
+	select @projectsCount =
+
+		  COUNT(*) from EmployeesProjects ep inner join Projects p on ep.ProjectId = p.Id where ep.EmployeeId = @epId;
+
+	if(@projectsCount > 1) return 1
+	return 0
 end;
